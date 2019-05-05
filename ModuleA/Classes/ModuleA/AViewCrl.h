97efefb727c5ae90c0d3f2f38518c4b2c902ce3d
hxw <1375166613@qq.com>
2019-05-05 11:24:48 +0800

    test A

---
+++ ModuleA/Classes/ModuleA/AViewCrl.h
@@ -0,0 +1,17 @@
+//
+//  AViewCrl.h
+//  ComponentDemo
+//
+//  Created by 华晓伟 on 2019/4/30.
+//  Copyright © 2019年 华晓伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AViewCrl : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ModuleA/Classes/ModuleA/AViewCrl.m
@@ -0,0 +1,35 @@
+//
+//  AViewCrl.m
+//  ComponentDemo
+//
+//  Created by 华晓伟 on 2019/4/30.
+//  Copyright © 2019年 华晓伟. All rights reserved.
+//
+
+#import "AViewCrl.h"
+
+@interface AViewCrl ()
+
+@end
+
+@implementation AViewCrl
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSString *str = @"abcdefg";
+    NSString *newStr = [str substringToIndex:100];
+    NSLog(@"+++++++++++++++%@",newStr);
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
